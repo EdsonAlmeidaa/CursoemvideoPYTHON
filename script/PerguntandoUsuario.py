435f0d7f2a8642e49c807135620d14aae1d623ae
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-02-22 11:52:35 -0300

    Print de saida
    Informando o ano de nascimento.

--- script/PerguntandoUsuario.py
+++ script/PerguntandoUsuario.py
@@ -3,3 +3,5 @@ idade = input('Quantos anos você tem? ')
 peso = input('Quanto você pesa? ')
 
 print(f'O {nome} pesa {idade} kg e tem a idade de {peso} anos.')
+print(f'A(O) {nome} nasceu em {2021 - int(idade)}')
+
