5db048201dcc3cead9334795cf93abee1de79dc1
Edson Almeida <79293954+EdsonAlmeidaa@users.noreply.github.com>
2021-02-22 12:07:06 -0300

    Atualização do print de saida
    Fiz uma atualização do print de saida. Utilizando um cast:  Cast é a converção de um tipo de dado para outro.

--- script/PerguntandoUsuario.py
+++ script/PerguntandoUsuario.py
@@ -1,7 +1,7 @@
 nome = input('Qual é seu nome? ')
-idade = input('Quantos anos você tem? ')
+idade = int(input('Quantos anos você tem? '))
 peso = input('Quanto você pesa? ')
 
 print(f'O {nome} pesa {idade} kg e tem a idade de {peso} anos.')
-print(f'A(O) {nome} nasceu em {2021 - int(idade)}')
+print(f'A(O) {nome} nasceu em {2021 - idade}')
 
